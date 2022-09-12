086b77ad6f59dbc59cb03ce703b7e312f9e98a8f
DTFV43 <Vikas.Sahu@motorolasolutions.com>
2022-09-12 21:48:35 +0530

    Create project for jenkins

---
+++ firstProjectForJenkins/src/test/java/firstProjectForJenkins/FirstClassForJenkins.java
@@ -0,0 +1,13 @@
+package firstProjectForJenkins;
+
+public class FirstClassForJenkins {
+
+	public static void main(String[] args) {
+
+		
+		
+		
+		System.out.println("first small code gor jenkins");
+	}
+
+}
